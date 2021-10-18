b7f3183046c96a2ad6b9bb14f5a41c57c2ae60dc
mustafakusdul <30833783+mustafakusdul@users.noreply.github.com>
2021-10-18 17:53:12 +0300

    Add files via upload

---
+++ todos.js
@@ -0,0 +1,39 @@
+const btn = document.querySelector('.btn')
+const myList = document.querySelector('.myList')
+const inputz = document.querySelector('.icnputz')
+
+btn.addEventListener("click", addNewItem);
+inputz.addEventListener("keyup", onEnterKey);
+
+function onEnterKey(e) {
+    if (e.keyCode === 13) {
+        e.preventDefault();
+        document.getElementById("btnX").click();
+    }
+}
+
+function addNewItem(e) {
+    e.preventDefault();
+
+    if (inputz.value === "") {
+        alert("Please add a new to do")
+    }
+
+    else {
+        const li = document.createElement("li");
+        const div = document.createElement("div")
+        const span = document.createElement('span')
+        const ahref = document.createElement("a")
+        li.innerText = inputz.value;
+        myList.appendChild(div);
+        div.appendChild(li)
+        div.appendChild(span)
+        span.classList = "spane"
+        li.classList = "myLi"
+        span.innerHTML = '<i class="fas fa-trash-alt"></i>'
+        div.classList = "item-colon"
+        inputz.value = ""
+    }
+}
+
+
